559b66ea1d8ad5a0010348891a8b16709e759e71
prem devasi <146634941+premdevasi@users.noreply.github.com>
2024-03-11 17:08:13 +0000

    variables config done

---
+++ 01_chaiorcode/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14445;
+let accountEmail = "officialprem@9509";
+var accountPassword = "12334";
+accountCity = "Jaipur";
+
+//accountId = 2//we cant change const key vraibles
+accountEmail = "prem@gmail.com";
+accountPassword = "815451";
+accountCity = "Barmer";
+console.log(accountId);
+
+
+
+
+
