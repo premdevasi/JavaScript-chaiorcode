d2682b9127737133265cfc208883aa749d80cfdb
prem devasi <146634941+premdevasi@users.noreply.github.com>
2024-03-12 10:00:32 +0000

    conversion of datatype

--- 01_chaiorcode/03_conversionOperation.js
+++ 01_chaiorcode/03_conversionOperation.js
@@ -6,6 +6,14 @@ let score  = "33adfv";
  //console.log(typeof valueInNumber);
  console.log(valueInNumber);
 
- let isLogedin = 1
+//  let isLogedin = 1
+//  let booleanisLogedin = Boolean(isLogedin);
+//  console.log(booleanisLogedin);
+ let isLogedin = "true"
  let booleanisLogedin = Boolean(isLogedin);
  console.log(booleanisLogedin);
+
+ let someNumber =  33;
+ let strinSomeNumber = String(someNumber)
+ console.log(strinSomeNumber);
+ console.log(typeof strinSomeNumber);
