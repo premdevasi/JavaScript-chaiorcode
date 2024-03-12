37f1d2ae9ad726cc4df7a4b570711e25ecd0bca4
prem devasi <146634941+premdevasi@users.noreply.github.com>
2024-03-12 07:32:38 +0000

    01_varialbles video complete

--- 01_chaiorcode/01_variables.js
+++ 01_chaiorcode/01_variables.js
@@ -2,12 +2,20 @@ const accountId = 14445;
 let accountEmail = "officialprem@9509";
 var accountPassword = "12334";
 accountCity = "Jaipur";
+let accountState;
 
 //accountId = 2//we cant change const key vraibles
 accountEmail = "prem@gmail.com";
 accountPassword = "815451";
 accountCity = "Barmer";
 console.log(accountId);
+
+/*
+Prefer not use to var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
 
 
