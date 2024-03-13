0a0f68e4ba7e9b8283b064e2942c9fd56db17433
prem devasi <146634941+premdevasi@users.noreply.github.com>
2024-03-13 05:16:23 +0000

    04_comparison.js done

---
+++ 01_chaiorcode/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log("2">1);//autometacli 2 convert in number and output will be true
+// console.log("02">1);//autometacli 2 convert in number and output will be true
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+console.log("2"===1);
