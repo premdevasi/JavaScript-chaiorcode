63d0229a3993b39c50928d1dfb433f937b1ebfbc
prem devasi <146634941+premdevasi@users.noreply.github.com>
2024-03-13 05:01:38 +0000

    03_conversionOperation.js done

--- 01_chaiorcode/03_conversionOperation.js
+++ 01_chaiorcode/03_conversionOperation.js
@@ -1,19 +1,45 @@
 let score  = "33adfv";
- console.log(typeof score);
- console.log(typeof (score));
+//  console.log(typeof score);
+//  console.log(typeof (score));
 
  let valueInNumber = Number(score)
  //console.log(typeof valueInNumber);
- console.log(valueInNumber);
+ //console.log(valueInNumber);
 
 //  let isLogedin = 1
 //  let booleanisLogedin = Boolean(isLogedin);
 //  console.log(booleanisLogedin);
  let isLogedin = "true"
  let booleanisLogedin = Boolean(isLogedin);
- console.log(booleanisLogedin);
+ //console.log(booleanisLogedin);
 
  let someNumber =  33;
  let strinSomeNumber = String(someNumber)
- console.log(strinSomeNumber);
- console.log(typeof strinSomeNumber);
+//  console.log(strinSomeNumber);
+//  console.log(typeof strinSomeNumber);
+
+/****************************** Oprecation  ********************/
+let value = 33;
+let nagValue = -value;
+//console.log(nagValue);
+
+let str1 = "hello"
+let sttr2 = " prem"
+
+// let str3 = str1+sttr2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter);
