11605eed282584ddd70c7a5c4e150743bb24469b
prem devasi <146634941+premdevasi@users.noreply.github.com>
2024-03-18 04:30:09 +0000

    function is Done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -33,7 +33,7 @@ const result = addTwoNumber(3, 5);
 
 // function loginUserMessage(username){
 //     if(username === undefined){
-//         console.log("please a enter username");
+//         console.log("please  enter username");
 //         return
 //     }
 // return `${username} just logged in`//we can also return like this
@@ -51,4 +51,39 @@ return `${username} just logged in`//we can also return like this
 }
 
 //console.log(loginUserMessage("prem"))
-console.log(loginUserMessage())//jab arguments me value nhi likhate to undefined
+//console.log(loginUserMessage())//jab arguments me value nhi likhate to undefined
+
+
+
+// function calculateCartPrice(...num1){//...(isko rest oprator kahte he)  likhane ke bad arguments ham kintni bhi value pass karva sakte he
+// return num1;
+// }
+// console.log(calculateCartPrice(200,300,780,410));
+
+function calculateCartPrice(val1,val2,...num1){
+     return num1;
+    }
+     //console.log(calculateCartPrice(200,300,780,410));//output =[780,410] val1=200,val2=300, to output 780,410 aayega
+
+const user={
+    userName:"prem",
+   prices:199,//pricce ko agar prices likha output = user is prem and price is undefined aayega.
+} 
+
+function handleObject(anyObject){
+//console.log(`user is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+
+// handleObject(user);
+ handleObject({
+    userName:"sam",
+    price:399,
+ });
+
+ //Arrays in function
+ const myNewArray = [200,300,400,785,412,551,626]
+ function returnSecondValue(getArray){
+ return getArray[1]
+ }
+ //console.log(returnSecondValue(myNewArray));//output = 300
+ console.log(returnSecondValue([200,300,400,785,412,551,626]));//ham ese bhi pass kar sakte he
